fe863fdb01807c265390b9090ebb98ac338c1ff7
awadad <adadada@gmail.com>
2023-09-26 09:46:30 +0200

    koniec lekcji wtorek

--- Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
+++ Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
@@ -2,7 +2,19 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   //Napisz program który wczyta liczbę od użytkownika i ją wyświelti na konsoli.
+
+
+	std::cout << "Podaj liczbe\n";
+	int numberFromUser;
+	std::cin >> numberFromUser;
+
+	std::cout << "Podales "<< numberFromUser << "\n";
+
+
+
+
+
 }
 
 
@@ -11,5 +23,45 @@ Algorytm-zbiór instrukcji, który rozwiązuje zadany problem.
 Określa też kolejność wykonywanych instrukcji
 Zapis algorytmu:
 *rysunek
+*w punktach
+* schemat budowy
+* kod źródłowy danego jeżyka programowania
+* pseudokod
+
+
+Zmienna - pewien obszar w pamięci operacyjnej, w której można w denej chwili przechować tylko jedną daną.
+
+Instrukcja deklaracji zmiennej
+typ_zmiennej nazwa_zmiennej;
+
+
+Typ zmiennej - wielkość obszsau pamięci, interpretacja ciągu bitów
+int- liczba całkowita ze zankiem <-2 147 483 648, 2 147 483 647>
+short - 2 bajtowa liczba całkowita ze znakiem <-32 768, 32 767>
+long - to comao co int 
+long long - 8 bajtowa liczba ze znakiem <-9 223 372 836m 854 775 888, 9 223 272 836 854 775 887>
+
+unsigned - zmienna bez znajku <8, 2*max.
+float - bajtowa liczba rzeczywista, dokładność 6-7 cyfr po przecinku 
+dounle - 8 bajtowa liczba rzeczywista, dokładność 15-16 cyfr po przecinku 
+long double - 12 bajtowa liczba rzeczywista, dokładność 19-20 cyfr po przecinku 
+Nazwa zmiennej - nazwa obszaru pamięci, identyfikator  
+Warunki niezbędne:
+* dozwoleone znaki:
+   - alfabert angielski
+   -cyfry arabskie 0-9
+   -podkreślenie (podłoga)
+* pierwszym znkiem nie może byc cyfra
+* unikalny w swoim zalkresie widoczności 
+* nie może to być słowo kluczowe (zarezerwowne) danego języka 
+
+Warunki programistów:
+* nazwa zmiennej powinna dawac charakter wykonywanych danych 
+* jeśli wiele słów w  miejscu sapcji wstawiamy podkreślenie albozaczynając od drugiego słowa piszemy je z dużej litry 
+* piszemy po angielsku
+*
+
+
+
 
 */

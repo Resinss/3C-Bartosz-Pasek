02f127e646f6c71c17b6c3b960dac54be6ea207a
awadad <adadada@gmail.com>
2023-10-03 09:52:40 +0200

    koniec lekcji 03.10.2023

--- Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
+++ Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
@@ -1,21 +1,94 @@
 ﻿#include <iostream>
 
-int main()
+/*
+*. Program obliczający pole prostokąta. 3
+*. Program obliczający objętość stożka. 4
+*. Program obliczający pole koła.
+*. Program obliczający wartość wyrażenia a^2 + b^2
+*. Program obliczający pole trójkąta o podstawie b i wysokości h
+*. Program obliczający objętość kuli o promieniu r
+*. Program obliczający pole trapezu o podstawach a i b oraz wysokości h
+*. Program obliczający średnią ważoną trzech liczb z podanymi odpowiednimi wagami w1, w2 i w3.
+*. Stwórz program, który pobiera od użytkownika długość w metrach i przelicza ją na centymetry i milimetry, wyświetlając wynik.
+*. Napisz program, który konwertuje wprowadzoną kwotę w jednej walucie na inną, korzystając z określonego kursu wymiany.
+*/
+
+//Napisz program który wczyta liczbę od uzytkownika i ją wkleji 
+
+void task1()
 {
-   //Napisz program który wczyta liczbę od użytkownika i ją wyświelti na konsoli.
-
-
+    int numberFromUser;
 	std::cout << "Podaj liczbe\n";
-	int numberFromUser;
-	std::cin >> numberFromUser;
+	std::cin  >> numberFromUser;
 
-	std::cout << "Podales "<< numberFromUser << "\n";
+	std::cout << "Podales " << numberFromUser << "\n";
+}
+
+
+//Program obliczający średnią artymatyczną dwóch liczb.
+void task2()
+{
+	std::cout << "Podaj liczbę pierwszą\n";
+	int firstNumber, secondNumber;
+	std::cin >> firstNumber;
+	std::cout << "Podaj drugą liczbę\n";
+	std::cin >> secondNumber ;
+	
+	int avarge;
+	avarge = (firstNumber + secondNumber) / 2;
+
+	std::cout << "Średnia to: " << avarge << "\n";
+}
+
+
+
+
+void task3()
+{
+	int pierwszy_bok, drugi_bok;
+	std::cout << "Podaj pierwszy bok\n";
+	std::cin >> pierwszy_bok;
+	std::cout << "Podaj drugi bok\n";
+	int secondNumber;
+	std::cin >> drugi_bok;
+	int area;
+	area = pierwszy_bok * drugi_bok;
+	std::cout << "Pole wynosi:";
+    std::cout << area;
+}
+
+void task4()
+{
+	int cone_base, cone_height;
+	std::cout << "Podaj podstawe stożka\n";
+	std::cin >> cone_base;
+	std::cout << "Podaj wysokość stożka\n";
+		std::cin >> cone_base;
+		int area;
+		area = 1/3*cone_height*3.14*r*r;
+		std::cout << "Objętość wynosi:";
+		std::cout << area;
+
+
 
 
 
 
 
 }
+
+
+
+
+int main()
+{
+	setlocale(LC_CTYPE, "polish");
+	//task1();
+	//task2();
+	//task3();
+	task4();
+}
+
 
 
 /*

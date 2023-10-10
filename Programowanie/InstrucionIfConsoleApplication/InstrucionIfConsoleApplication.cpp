a816fb889148ceccf3c43f7da36f974b6b65f349
awadad <adadada@gmail.com>
2023-10-10 09:51:06 +0200

    koniec 3 lekcji wtorek

---
+++ Programowanie/InstrucionIfConsoleApplication/InstrucionIfConsoleApplication.cpp
@@ -0,0 +1,32 @@
+﻿
+
+#include <iostream>
+
+//Napisz program, który wyświetli informacje czy liczba jest dodatnia czy nie.
+void task1()
+{
+	int number;
+	std::cout << "Podaj liczbe:\n";
+	std::cin >> number;
+
+	if (number > 0)
+		std::cout << "Liczba jest dodatnia\n";
+	
+	if (number < 0)
+		std::cout << "Liczba jest ujemna\n";
+
+	if (number == 0)
+		std::cout << "Liczba jest rowna zero\n";
+	
+	std::cout << "Koloejna instrukcja\n";
+}
+
+
+
+int main()
+{
+	task1();
+
+
+
+}

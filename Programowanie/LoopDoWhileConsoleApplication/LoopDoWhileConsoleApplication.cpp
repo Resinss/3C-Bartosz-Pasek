f282df94709bd7f5022b297a58aa5f7045bde52e
awadad <adadada@gmail.com>
2023-11-14 08:33:44 +0100

    koniec lekcji

--- Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
+++ Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
@@ -122,15 +122,113 @@ void task4()
 
 void task5()
 {
+	int number;
+	std::cout << "Podaj liczbê\n";
+	std::cin >> number;
 
+	int sum = 0;
+	int rest;
+
+	/*
+	rest = number % 10;
+	sum = sum + rest;
+	number = number / 10;
+	if (number != 0)
+	{
+		rest = number % 10;
+		sum = sum + rest;
+		number = number / 10;
+		if (number != 0)
+		{
+			rest = number % 10;
+			sum = sum + rest;
+			number = number / 10;
+			if (number != 0)
+			{
+				rest = number % 10;
+				sum = sum + rest;
+				number = number / 10;
+				//if (number != 0) ...
+			}
+		}
+	}
+	*/
+
+	do
+	{
+		rest = number % 10;
+		sum = sum + rest;
+		number = number / 10;
+	} while (number != 0);
+
+	std::cout << "Suma " << sum << "\n";
+	//4125
 }
+
+//* Poproœ u¿ytkownika o podawanie liczb, a¿ wprowadzi zero.
+//Oblicz sumê oraz œredni¹ artymetyczn¹ wprowadzonych liczb.
+
+void task6()
+{
+	int number;
+	int sum = 0;
+	int numberOfNumbers = 0;
+
+	std::cout << "Podaj liczbe:\n";
+	std::cin >> number;
+	sum = sum + number;
+	numberOfNumbers++;
+	if (number != 0)
+	{
+		std::cout << "Podaj liczbe:\n";
+		std::cin >> number;
+		sum = sum + number;
+		numberOfNumbers++;
+		if (number != 0)
+		{
+			std::cout << "Podaj liczbe:\n";
+			std::cin >> number;
+			sum = sum + number;
+			numberOfNumbers++;
+			if (number != 0)
+			{
+				std::cout << "Podaj liczbe:\n";
+				std::cin >> number;
+				sum = sum + number;
+				numberOfNumbers++;
+				if (number != 0);
+			}
+		
+		
+		
+		}
+	
+
+
+
+	}
+ 
+	std::cout << "Suma liczb wynosi " << sum << "\n";
+	double avg = sum / numberOfNumbers;
+	std::cout << "Œrednia " << avg << "\n";
+
+	do
+	{
+		std::cout << "Podaj liczbê:\n";
+		std::cin >> number;
+		sum = sum + number;
+		numberOfNumbers++;
+	} while (number != 0);
+}
+
 
 int main()
 {
 	//task1();
 	//task2();
 	//task3();
-	task4();
+	//task4();
 	//task5();
+	task6();
 }
 

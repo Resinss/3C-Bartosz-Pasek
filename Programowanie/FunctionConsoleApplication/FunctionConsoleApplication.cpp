427f12a53f60019bc2eaf058767cf85ea17fad3b
awadad <adadada@gmail.com>
2024-01-31 08:58:51 +0100

    koniec lekcji

--- Programowanie/FunctionConsoleApplication/FunctionConsoleApplication.cpp
+++ Programowanie/FunctionConsoleApplication/FunctionConsoleApplication.cpp
@@ -40,16 +40,75 @@ void task3(std::string name, int age)
         std::cout << name << "jestem nie po³noletni/pe³noletnia\n";
 }
 
+
+//Napisz program, który przestestuje zachowanie siê przekazywanych danych przez paraetr
+void task4(int number)
+{
+    std::cout << "zmienna number w funkcji task4 :" << number << "\n";
+    number--;
+    std::cout << "zmienna number w funkcji task4 :" << number << "\n";
+}
+
+//napisz program który wykorzysta przekazywanie parametru pprzez referencje
+void task5(int& number)
+{
+    std::cout << "zmienna number w funkcji task5 :" << number << "\n";
+    number--;
+    std::cout << "zmienna number w funkcji task5 :" << number << "\n";
+}
+
+//Napisz funkcje ktora pobierze od u¿ytkownuika liczbe
+void task6_GetNumber(int& number)
+{
+    std::cout << "podaj liczbe: \n";
+    std::cin >> number;
+}
+
 int main()
 {
+    setlocale(LC_CTYPE, "polish");
+    /*task1();
     task1();
-    task1();
+    */
+   
+    /*
     task2("witaj swiecie!!");
     task2("ala ma kota");
     std::string text = "uczyæ sie programowania!!";
     task2(text);
-
-    task3("jan", 35);
+    */
+    
+    /*task3("jan", 35);
     task3("ala", 17);
+    */
+   
+    /*
+    int number = 5;
+    std::cout << "zmienna number w funkcji main :" << number << "\n";
+    task4(number);
+    std::cout << "zmienna number w funkcji task4 :" << number << "\n";
+
+    task4(number);
+    std::cout << "zmienna number w funkcji main :" << number << "\n";
+
+    task4(9);
+    const int NUMBER = 9;
+    task4(NUMBER);  
+    */
+
+    /*
+    int number = 5;
+    const int NUMBER = 9;
+    std::cout << "zmienna number w funkcji main :" << number << "\n";
+    task5(number);
+    std::cout << "zmienna number w funkcji task4 :" << number << "\n";
+
+    //task5(9); //BLAD - przez parametr mozna przekazac tylko zmienna
+    //task5(NUMBER); //BLAD - przez parametr mozna przeakazac tylko zmienna
+    */
+
+    int numberFromUser = 5;
+    task6_GetNumber(numberFromUser);
+    std::cout << "u¿ytkownik podal liczbe " << numberFromUser << "\n";
 }
 

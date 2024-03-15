3c3d4229e9cfebbd3a6daadc9eb2aa72323eb728
awadad <adadada@gmail.com>
2024-03-15 09:13:46 +0100

    .

---
+++ Programowanie/TasksConsoleApplication/TasksConsoleApplication.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+/*
+Zadanie: Tworzenie prostego kalkulatora
+
+Napisz program, który bêdzie dzia³a³ jako prosty kalkulator
+i bêdzie umo¿liwia³ u¿ytkownikowi wykonywanie
+podstawowych operacji arytmetycznych na dwóch liczbach.
+
+Instrukcje:
+
+Poproœ u¿ytkownika o podanie dwóch liczb oraz wybór operacji (dodawanie, odejmowanie, mno¿enie lub dzielenie).
+W zale¿noœci od wyboru u¿ytkownika, wykonaj odpowiedni¹ operacjê arytmetyczn¹.
+Wyœwietl wynik operacji.
+Przyk³adowe dzia³anie programu:
+
+Prosty Kalkulator
+-----------------
+1. Dodawanie
+2. Odejmowanie
+3. Mno¿enie
+4. Dzielenie
+
+Wybierz operacjê (1/2/3/4): 3
+Podaj pierwsz¹ liczbê: 5
+Podaj drug¹ liczbê: 3
+
+Wynik mno¿enia: 15
+*/
+
+void task1()
+{
+    std::cout << "aby odejmowaæ wybierz 1 \n";
+    std::cout << "aby dodawaæ wybierz 2 \n";
+    std::cout << "aby dzieliæ wybierz 3 \n";
+    std::cout << "aby mno¿yæ wybierz 4 \n";
+    std::cin >>
+}
+
+int main()
+{
+    task1();
+
+}
+

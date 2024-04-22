575bd78a68051f1511599816a2b63a20f705bec3
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-04-22 16:00:42 +0200

    kopnice

--- Programowanie/SkyConsoleApplication/SkyConsoleApplication.cpp
+++ Programowanie/SkyConsoleApplication/SkyConsoleApplication.cpp
@@ -2,11 +2,42 @@
 #include <windows.h>
 #include <conio.h>
 
-void task1()
+//Pojawienie si? gwiazdki znikni?cie i znowu pojawienie si? jej w innym miejscu 
+
+
+void setCursor(int x, int y)
+{
+	COORD c;
+	c.X = x;
+	c.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+
+void showConsoleCursor(bool showFlag)
+{
+	HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	CONSOLE_CURSOR_INFO     cursorInfo;
+
+	GetConsoleCursorInfo(out, &cursorInfo);
+	cursorInfo.bVisible = showFlag; // set the cursor visibility
+	SetConsoleCursorInfo(out, &cursorInfo);
+}
+
+void getConsolResolution(int& consoleWidth, int& consoleHeight)
+{
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+
+	consoleWidth = csbi.srWindow.Right - csbi.srWindow.Left;
+	consoleHeight = csbi.srWindow.Bottom - csbi.srWindow.Top;
+}
+
+int main()
 {
 	srand(time(0));
-	int consoleWidth = 30;
-	int consoleWidth = 34;
+	int consoleWidth = 55;
+	int consoleHeight = 34;
 
 	getConsolResolution(consoleWidth, consoleHeight);
 	showConsoleCursor(false);
@@ -17,11 +48,12 @@ void task1()
 		int x = rand() % consoleWidth;
 		int y = rand() % consoleHeight;
 
+		setCursor(x, y);
+		std::cout << "*";
+
+		Sleep(700);
+		setCursor(x, y);
+		std::cout << " ";
+
 	}
-
-}
-
-int main()
-{
-	task1;
 }

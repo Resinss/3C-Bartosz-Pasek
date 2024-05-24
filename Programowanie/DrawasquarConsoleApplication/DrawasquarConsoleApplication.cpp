8637378cf0d2f7b7e2b54a80116533863bcb6913
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-05-24 09:53:10 +0200

    koniec lekcji

--- Programowanie/DrawasquarConsoleApplication/DrawasquarConsoleApplication.cpp
+++ Programowanie/DrawasquarConsoleApplication/DrawasquarConsoleApplication.cpp
@@ -2,11 +2,37 @@
 #include <windows.h>
 #include <conio.h>
 
-enum keyCode
+void setCursor(int x, int y)
 {
-	UP,
-	DOWN,
-	LEFT,
-	RIGHT,
-	ESC
-};
+	COORD c;
+	c.X = x;
+	c.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+
+int main()
+{
+	int boardCoordinatesLeftTop = 20;
+	int boardLeftTopX = 20;
+	int boardLeftTopY = 20;
+	int boardRightBottomX = 80;
+	int boardRightBottomY = 40;
+
+
+	setCursor(boardCoordinatesLeftTop, boardLeftTopY);
+	for (int i = boardLeftTopX; i <= boardRightBottomX; i++)
+		std::cout << "#";
+
+	for (int row = boardLeftTopY + 1; row < boardRightBottomY; row++)
+	{
+		setCursor(boardLeftTopX, row);
+		std::cout << "#";
+
+		setCursor(boardRightBottomX, row);
+		std::cout << "#";
+	}
+
+	setCursor(boardLeftTopX, boardRightBottomY);
+	for (int i = boardLeftTopX; i <= boardRightBottomX; i++)
+		std::cout << "#";
+}

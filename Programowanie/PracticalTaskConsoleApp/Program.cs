3d0868f31bfe7d02efa906cee74f56097d6ceb62
Sala213 <Sala213@ezse.pl>
2025-10-30 16:52:10 +0100

    t

--- Programowanie/PracticalTaskConsoleApp/Program.cs
+++ Programowanie/PracticalTaskConsoleApp/Program.cs
@@ -1,7 +1,17 @@
 ﻿using PracticalAskConsoleApp.Styczeń_2023_1;
+using PracticalTaskConsoleApp.czerwiec_2022;
 
 //Styczeń 2023_1
 
-
+/*
 task1 task1 = new task1();
 task1.task();
+*/
+
+//czerwiec2022
+
+Person person = new Person(5, "Ktoś");
+person.WriteName("Marek");
+
+Person person2 = new Person(6, "Cos");
+person2.WriteName("Ktoś");
--- Programowanie/PracticalTaskConsoleApp/czerwiec_2022/task3.cs
+++ Programowanie/PracticalTaskConsoleApp/czerwiec_2022/task3.cs
@@ -6,8 +6,41 @@ using System.Threading.Tasks;
 
 namespace PracticalTaskConsoleApp.czerwiec_2022
 {
-    internal class task3
+    public class Person
     {
+        private static int _count = 0;
+        private int id;
+        private string firstName;
+        public int number_of_instance;
 
+        public Person()
+        {
+            id = 0;
+            firstName = string.Empty;
+            _count++;
+            number_of_instance = _count;
+        }
+
+        public Person(int id, string firstName)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            _count++;
+            number_of_instance = _count;
+        }
+
+        public Person(Person other)
+        {
+            id = other.id;
+            firstName = other.firstName;
+            _count++;
+            number_of_instance = _count;
+        }
+
+        public void WriteName(string who)
+        {
+            string displayName = string.IsNullOrWhiteSpace(firstName) ? "Brak danych" : firstName;
+            Console.WriteLine($"Cześć {who}, mam na imię {displayName}");
+        }
     }
 }
